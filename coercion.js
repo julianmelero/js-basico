20d484cb9e76cab2da4266c734655836da000a03
julianmelero <julian@melerohidalgo.es>
2020-08-26 10:37:15 +0200

    Coerción

---
+++ coercion.js
@@ -0,0 +1,17 @@
+// La coerción es poder cambiar valores de las variables, por ejemplo, de número a texto
+
+// Implícita
+
+4 +"7" // String 47
+
+4 * "2" // Number 8
+
+// Explícita
+
+var a = 20;
+var b = a + ""; 
+
+
+var c= String(a); 
+
+var d = Number(c);
