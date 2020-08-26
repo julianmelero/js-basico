8aedaa13e13c000906dc23373ecb134b0ff9ba90
julianmelero <julian@melerohidalgo.es>
2020-08-26 11:37:45 +0200

    operadores

---
+++ operadores.js
@@ -0,0 +1,27 @@
+// Operadores Binarios
+
+// + --> 3+2
+// -  --> 50- 10
+// * --> 10 * 20
+// / --> 20 / 2
+
+
+// Operador Unitario
+// ! -->  Negación    !True (False)
+
+// Operadores de asignación
+// = --> var num = 1;
+
+// Operadores de comparación
+// == Compara el valor --> 3 == "3" (True)
+// === Compara el tipo de valor y su valor --> 3 == "3" (False)
+// <    --> 5 < 3 (False)
+// >    --> 5 > 3 (True)
+// <=   --> 5 <= 3 (False)
+// >=   --> 5>=5  (True)
+
+// &&   --> a && b   Si a y b es cierto
+// ||   --> a || b  Si a o b es cierto
+
+// ++   --> edad++ Incrementa edad en 1
+// +=   --> edad +=2 Incrementa edad en 2
