f26ced8b7e6d32943dd1a998612e546fd7b3a6d8
julianmelero <julian@melerohidalgo.es>
2020-08-26 12:43:36 +0200

    If else

---
+++ if.js
@@ -0,0 +1,21 @@
+// Condicionales
+
+var edad = 17;
+
+if (edad >= 18) {
+    console.log("Mayor de edad");
+} else if(edad <= 5) {
+    console.log("Eres un Baby")
+}
+ else {
+    console.log("Eres menor de edad");
+}
+
+
+// Operador ternario
+// Genera if else en una línea
+
+
+// condicion ? true : false;
+
+edad >= 18 ? console.log("Mayor de edad") : console.log("Menor de edad");
