d57baab7544e109691d2e6c87a47abd11d6f15a2
julianmelero <julian@melerohidalgo.es>
2020-08-26 11:20:51 +0200

    Truthly and Falsy

---
+++ truthy_and_Falsy.js
@@ -0,0 +1,21 @@
+// Valores por defecto False
+
+// El número 0
+// Null
+// NaN
+// undefined
+// False
+// String vacío ""
+
+
+// Valores True
+
+// Espacios " "
+// >= 1
+// Arrays
+// Objetos
+// Funciones
+
+
+
+
