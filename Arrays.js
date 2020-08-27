090da6887527cbcd9e23290ea9026822f99955e2
julianmelero <julian@melerohidalgo.es>
2020-08-27 12:58:33 +0200

    Filter Method

--- Arrays.js
+++ Arrays.js
@@ -24,3 +24,22 @@ frutas.shift();
 
 // Conocer la posición
 frutas.indexOf("Cereza"); // 2
+
+// Métodos Recorrido
+
+var articles = [
+    { nombre: "Bici", costo: 3000},
+    { nombre: "TV",   costo: 2500},
+    { nombre: "Libro",   costo: 320},
+    { nombre: "Celular",   costo: 10000},
+    { nombre: "Laptop",   costo: 20000},
+    { nombre: "Teclado",   costo: 500},
+    { nombre: "Audífonos",   costo: 1700},
+]
+
+// Filter
+var filtro = articles.filter(function(article){ 
+    return article.costo <= 500;
+});
+
+filtro;
