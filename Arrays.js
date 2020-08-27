016a8cc5acc92108c40d6971109317e734de27b7
julianmelero <julian@melerohidalgo.es>
2020-08-27 12:11:47 +0200

    Basic Array

---
+++ Arrays.js
@@ -0,0 +1,26 @@
+// Declaración
+
+var frutas = ["Manzana", "Plátano", "Cereza", "Fresa"];
+
+
+// Recoger datos
+consologe.log(frutas);
+frutas[0];
+
+// Longitud de un Array
+frutas.length();
+
+// Método Agregar al Array
+frutas.push("Uva");
+
+// Quitar último elemento
+frutas.pop();
+
+// Agregar elemento al inicio
+frutas.unshift("Sandia");
+
+// Eliminar elemento del inicio
+frutas.shift();
+
+// Conocer la posición
+frutas.indexOf("Cereza"); // 2
