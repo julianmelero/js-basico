f5855e520fc81f49bad042bb636e8cb3c7f253cd
julianmelero <julian@melerohidalgo.es>
2020-08-27 12:49:29 +0200

    Constructor

---
+++ Constructor.js
@@ -0,0 +1,11 @@
+function auto(marca,modelo,anyo) {
+    this.marca  = marca;
+    this.modelo = modelo;
+    this.anyo   = anyo;
+}
+
+//  Nuevo objeto
+var AutoNuevo = new auto("Toyota", "Corola", 2020);
+
+AutoNuevo.modelo;
+
