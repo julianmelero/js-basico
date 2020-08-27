f886ccee89e7a6cb24aa272b32967401dd2c8599
julianmelero <julian@melerohidalgo.es>
2020-08-27 10:01:27 +0200

    Switch

---
+++ switch.js
@@ -0,0 +1,15 @@
+var edad = 20;
+
+switch (edad) {
+    case 20:
+        console.log("Tu edad es 20 ");        
+        break;
+    case 30:
+        console.log("Tu edad es 30 ");
+        break;
+    case 40:
+        console.log("Tu edad es 40 ");
+    default:
+        console.log("No reconocemos tu edad");
+        break;
+}
